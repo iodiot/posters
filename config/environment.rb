1c6e21032fc992e4382c0a395154f977d754b2c3
iodiot <iodiot@gmail.com>
2013-05-14 11:54:14 +0300

    No application log

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-Rails.logger = Logger.new("log/application")
-
 # Initialize the rails application
 Posters::Application.initialize!
